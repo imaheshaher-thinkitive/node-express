3802ada7492b6895514386fbc8faed29c69cdcd1
imaheshaher-thinkitive <mahesh.aher@thinkitive.com>
2022-03-10 20:10:19 +0530

    User profile address

--- app.js
+++ app.js
@@ -38,26 +38,8 @@ app.get("/",async(req,res) =>{
     return res.send("hello")
 })
 
-app.use((err, req, res, next) => {
-  let responseStatusCode = 500;
-  let responseObj = {
-    success: false,
-    data: [],
-    error: err,
-    message: 'There was some internal server error',
-  };
-
-  // IF THERE WAS SOME ERROR THROWN BY PREVIOUS REQUEST
-  if (!_.isNil(err)) {
-    // IF THE ERROR IS REALTED TO JWT AUTHENTICATE, SET STATUS CODE TO 401 AND SET A CUSTOM MESSAGE FOR UNAUTHORIZED
-    if (err.name === 'JsonWebTokenError') {
-      responseStatusCode = 401;
-      responseObj.message = 'You cannot get the details. You are not authorized to access this protected resource';
-    }
-  }
-
-  if (!res.headersSent) {
-    res.status(responseStatusCode).json(responseObj);
-  }
-});
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.json({ error: err });
+//   });
 app.listen(env.PORT,()=>console.log(`App listinig on port ${env.PORT}`))
